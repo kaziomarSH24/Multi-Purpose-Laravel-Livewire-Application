b0827e5076b0df27abf223319b3ced9e6d4235c8
Kazi Omar Faruk <kaziomar144@gmail.com>
2022-07-28 23:56:41 +0600

    Learn about how to deleted livewire tmp file automatically that are 24hrs & how we can modify this

--- app/Http/Livewire/Admin/Profile/UpdateProfile.php
+++ app/Http/Livewire/Admin/Profile/UpdateProfile.php
@@ -24,6 +24,25 @@ class UpdateProfile extends Component
         $this->dispatchBrowserEvent('updated',['message' => 'Profile image changed successfully!']);
     }
 
+    protected function cleanupOldUploads()  //Livewire tmp file delete automatically
+    {
+    
+        $storage = Storage::disk('local');
+
+        // dd($storage->allFiles('livewire-tmp'));
+
+        foreach ($storage->allFiles('livewire-tmp') as $filePathname) {
+            // On busy websites, this cleanup code can run in multiple threads causing part of the output
+            // of allFiles() to have already been deleted by another thread.
+            if (! $storage->exists($filePathname)) continue;
+
+            $yesterdaysStamp = now()->subSecond(10)->timestamp;
+            if ($yesterdaysStamp > $storage->lastModified($filePathname)) {
+                $storage->delete($filePathname);
+            }
+        }
+    }
+
     public function render()
     {
         return view('livewire.admin.profile.update-profile');
